5b074bc68acf654e5488585887efb1f6e9679ba0
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-06 08:41:03 +0100

    correzione rotte

--- routers/posts.js
+++ routers/posts.js
@@ -4,28 +4,28 @@ const router = express.Router();
 
 // Rotte CRUD
 // index
-router.get('/posts', function (req, res) {
+router.get('/', function (req, res) {
 	res.send(`Lista dei post`);
   res.json(posts);
 });
 // show
-router.get('/posts/:id', function (req, res) {
+router.get('/:id', function (req, res) {
 	res.send(`Dettagli dei post` + req.params.id);
 });
 //store
-router.post('/posts', function (req, res) {
+router.post('/', function (req, res) {
 	res.send(`Creazione nuovo post`);
 });
 //update
-router.put('/posts/:id', function(req, res) {
+router.put('/:id', function(req, res) {
 	res.send(`Modifica integrale del post` + req.params.id);
 });
 //modify
-router.patch('/posts/:id', function(req, res) {
+router.patch('/:id', function(req, res) {
 	res.send(`Modifica parziale del post` + req.params.id);
 });
 //destroy
-router.delete('/posts/:id', function (res, req) {
+router.delete('/:id', function (res, req) {
   res.send(`Cancellazione del post` + req.params.id);
 });
 
